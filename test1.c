3da83b6643876698136fecf73c211cfdadc15fb7
ArchyLi <254771771@qq.com>
2016-12-13 23:57:03 +0800

    修改test
    同

--- test1.c
+++ test1.c
@@ -1,6 +1,9 @@
+    // 请实现字符串循环右移函数。列如字符串“abcdefghi”向右移2位就是“hiabcdefg”  
+    // 函数原型为：void RightLoopMove(char *pstr,unsigned short steps)  
     #include <stdio.h>  
     #include <assert.h>  
     #include <string.h>  
+    void severse(char *p,char *q)       //字符串翻转  
     {  
         char temp;  
         assert(p);  
@@ -17,6 +20,9 @@
     void RightLoopMove(char *pstr,unsigned short steps)  
     {  
         int len=strlen(pstr);  
+        severse(pstr,pstr+len-steps-1);       //前部分翻转  
+        severse(pstr+len-steps,pstr+len-1);   //后部分翻转  
+        severse(pstr,pstr+len-1);             //整体翻转  
     }  
     int main()  
     {  
